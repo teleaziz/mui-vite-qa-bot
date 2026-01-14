81248ec1f2a64e1552fe6da6f0eed71acc7479e8
Aziz Abbas <teleaziz@users.noreply.github.com>
2026-01-14 16:19:31 +0400

    Update Customers.tsx

--- src/crm/pages/Customers.tsx
+++ src/crm/pages/Customers.tsx
@@ -8,7 +8,7 @@ export default function Customers() {
   return (
     <Box sx={{ width: "100%", maxWidth: { sm: "100%", md: "1700px" } }}>
       <Typography variant="h4" component="h1" sx={{ mb: 4 }}>
-        Customers
+        Customers page
       </Typography>
 
       <Paper
